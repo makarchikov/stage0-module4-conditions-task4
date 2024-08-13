cfd18569199801bec3b579546973aa04ad823f75
salvadordali99 <152178723+salvadordali99@users.noreply.github.com>
2024-08-13 20:14:00 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = first;
+
+        if (second > max) {
+            max = second;
+        }
+
+        if (third > max) {
+            max = third;
+        }
+
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = first;
+        max = second > max ? second : max;
+        max = third > max ? third : max;
+        System.out.println(max);
     }
 }
